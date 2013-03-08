d77f81ec65d22bb522226eaba9aad0c3ae313259
Bogdan Zinko <bogdan25@hotmail.com>
2013-03-07 23:04:11 -0500

    update

--- 2012-10-24/addClassBody.js
+++ 2012-10-24/addClassBody.js
@@ -1,3 +1,5 @@
+<strong>Scenario:</strong> Adding a class on the body tag, when the section menu has a unique class.
+
 var properties = ["class1", "class2", "class3", "class4"]; 
         for(i=0;i<properties.length;i++){
 	activeOrNot(properties[i]);
